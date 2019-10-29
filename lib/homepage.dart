128108b42323b2ad91df17dfa87ee0ed54a68367
Hasimy <ahmadhasyimi674@gmail.com>
2019-10-29 13:04:06 +0700

    finalize upload

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -49,4 +49,87 @@ class HomepageState extends State<Homepage> {
       t2.text = '0';
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Flutter Calculator'),
+      ),
+      body: new Container(
+        padding: const EdgeInsets.all(40.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              "Output : $sum",
+              style: new TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red),
+            ),
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: "Enter a number"),
+              controller: t1,
+            ),
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: "Enter a number"),
+              controller: t2,
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("+"),
+                  color: Colors.cyanAccent,
+                  onPressed: addition,
+                ),
+                new MaterialButton(
+                  child: new Text("-"),
+                  color: Colors.cyanAccent,
+                  onPressed: subtract,
+                ),
+              ],
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("x"),
+                  color: Colors.cyanAccent,
+                  onPressed: multiply,
+                ),
+                new MaterialButton(
+                  child: new Text(":"),
+                  color: Colors.cyanAccent,
+                  onPressed: divide,
+                ),
+              ],
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("Clear"),
+                  color: Colors.cyanAccent,
+                  onPressed: clearing,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
